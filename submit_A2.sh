9f410d880313bd6b229db3c0b71b7441f38757d8
shli7029 <shli7029@uni.sydney.edu.au>
2021-05-25 21:53:53 +1000

    script

--- submit_A2.sh
+++ submit_A2.sh
@@ -1,5 +1,8 @@
 spark-submit \
     --master yarn \
     --deploy-mode client \
+    --num-executors 4 \
+    --executor-cores 4 \
+    --conf spark.default.parallelism = 64 \
     assignment2.py \
     --output $1
