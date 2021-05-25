ae86352476082008628117d9e979d60643c712c0
shli7029 <shli7029@uni.sydney.edu.au>
2021-05-25 16:22:16 +1000

    new commit

---
+++ submit_A2.sh
@@ -0,0 +1,5 @@
+spark-submit \
+    --master yarn \
+    --deploy-mode client \
+    assignment2.py \
+    --output $1
--- submit_cluster.sh
+++
@@ -1,5 +0,0 @@
-spark-submit \
-    --master yarn \
-    --deploy-mode cluster \
-    MovieData_Summary.py \
-    --output $1
